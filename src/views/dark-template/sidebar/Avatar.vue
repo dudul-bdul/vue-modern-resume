b8ed267c120bbf29edae4147fc10912fe8b1a842
Anticopyright <53624153+dudul-bdul@users.noreply.github.com>
2022-01-18 16:22:17 +0700

    Update Avatar.vue

--- src/views/dark-template/sidebar/Avatar.vue
+++ src/views/dark-template/sidebar/Avatar.vue
@@ -5,7 +5,7 @@
       size="120"
     >
       <img
-        :src="publicPath('/img/avatar.jpg')"
+        :src="publicPath('https://scontent.fbdo2-1.fna.fbcdn.net/v/t1.6435-9/176682723_291515495900609_7391343528824382938_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=973b4a&_nc_ohc=WqrT3WsKZdwAX8HbBCA&_nc_ht=scontent.fbdo2-1.fna&oh=00_AT99fI3xK2TQ1mE8yFcSkf4F8pSUCSfe_pg_6a9KeAvDNg&oe=620D14EC')"
         alt="A Praditia"
       >
     </v-avatar>
